0741989461cc4d8e31ea65404b974eaa19662aa5
momenma7rous <momenma7rous@gmail.com>
2025-07-27 01:20:28 +0300

    Create projects page & implement the api.

--- js/main.js
+++ js/main.js
@@ -12,3 +12,23 @@ closemenu.addEventListener("click", () => {
   menu.classList.remove("active")
   overlay.classList.remove("active")
 })
+
+// Fetch Projects From Github
+let projectsWrapper = document.querySelector("section.projects .wrapper")
+
+fetch('https://raw.githubusercontent.com/MomenMa7rous/pistachio-projects/refs/heads/main/projects.json')
+  .then(response => {
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    return response.json();
+  })
+  .then(data => {
+    console.log(data);
+    data.reverse().slice(0,4).forEach(project => {
+      let projectContainer = document.createElement("div")
+      projectContainer.classList.add("project")
+      projectContainer.innerHTML = `<img src="${project.img}" alt="${project.title}">`
+      projectsWrapper.appendChild(projectContainer)
+    })
+  })
---
+++ js/projects.js
@@ -0,0 +1,25 @@
+// Fetch data from Github
+let detailsWrapper = document.querySelector("section.projects .details")
+
+fetch('https://raw.githubusercontent.com/MomenMa7rous/pistachio-projects/refs/heads/main/projects.json')
+  .then(response => {
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    return response.json();
+  })
+  .then(data => {
+    data.forEach(project => {
+      let projectDetails = document.createElement("div")
+      projectDetails.classList.add("project")
+      projectDetails.innerHTML = `<div class="image">
+            <img src="${project.img}" alt="${project.title}">
+          </div>
+          <div class="info">
+            <h2>${project.title}</h2>
+            <p>${project.description}</p>
+            <span>${project.date}</span>
+          </div>`
+      detailsWrapper.appendChild(projectDetails)
+    })
+  })
